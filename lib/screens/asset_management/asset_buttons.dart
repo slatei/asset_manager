933be65accaa8ccf818bca75341c83ff1da20593
Nicholas Thorp <slatei.nic@gmail.com>
2024-08-18 16:21:52 +1200

    fix: Tidier app buttons

--- lib/screens/asset_management/asset_buttons.dart
+++ lib/screens/asset_management/asset_buttons.dart
@@ -4,11 +4,13 @@ class AssetButtons extends StatefulWidget {
   const AssetButtons({
     required this.tabController,
     required this.add,
+    this.remove,
     super.key,
   });
 
   final TabController tabController;
   final Function() add;
+  final Function()? remove;
 
   @override
   State<AssetButtons> createState() => _AssetButtonsState();
@@ -40,24 +42,39 @@ class _AssetButtonsState extends State<AssetButtons> {
       padding: const EdgeInsets.all(16.0),
       child: Row(
         children: [
+          // Navigation buttons
           OutlinedButton(
-            onPressed: _tabController.index != 0
-                ? () => _tabController.animateTo(_tabController.index - 1)
-                : null,
-            child: const Text('Back'),
-          ),
+              onPressed: _tabController.index != 0
+                  ? () => _tabController.animateTo(_tabController.index - 1)
+                  : null,
+              // child: const Text('Back'),
+              child: const Icon(Icons.keyboard_arrow_left)),
           const SizedBox(width: 10),
           OutlinedButton(
             onPressed: _tabController.index < _tabController.length - 1
                 ? () => _tabController.animateTo(_tabController.index + 1)
                 : null,
-            child: const Text('Next'),
+            // child: const Text('Next'),
+            child: const Icon(Icons.keyboard_arrow_right),
           ),
           const Expanded(child: SizedBox()),
-          FilledButton.icon(
+
+          // Delete button if delete function provided
+          if (widget.remove != null) ...[
+            OutlinedButton.icon(
+              onPressed: widget.remove,
+              label: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+            const SizedBox(width: 10),
+          ],
+
+          // Save button
+          FilledButton(
             onPressed: widget.add,
-            icon: const Icon(Icons.add, size: 18),
-            label: const Text('Add'),
+            child: const Text('Save'),
           ),
         ],
       ),
